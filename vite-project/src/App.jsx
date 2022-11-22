a085598192c67e48c9efce7dcdde2d602c692a5e
DanielAntunesRibeiro <d.a_ribeiro2@hotmail.com>
2022-11-22 19:16:07 -0400

    Criando componentes ReadAll e Create

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
-import Footer from './components/Header/Footer/Footer'
+import Create from './components/Create/Create'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import ReadAll from './components/ReadAll/ReadAll'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        
         <div className="content">
-          Conteúdo da página
+          <Routes>
+            <Route path='/' element={<ReadAll />} />
+            <Route path='/adicionar' element={<Create />} />
+          </Routes>
         </div>
+
         <Footer />
     </div>
   )
