fd8c1e084b2a90a664ae6e7a84fb9626d2b16960
DanielAntunesRibeiro <d.a_ribeiro2@hotmail.com>
2022-11-21 21:20:13 -0400

    implementando componente Footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Footer from './components/Header/Footer/Footer'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <div className="content">
+          Conteúdo da página
+        </div>
+        <Footer />
     </div>
   )
 }
---
+++ vite-project/src/components/Header/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer () {
+    return (
+        <div className="Footer">
+            Todos os direitos reservados
+        </div>
+    )
+}
+
+export default Footer
